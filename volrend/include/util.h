cc8c6037559e240eee4b66311ef5c9e3389b3268
Thais <thaiscamachoo@gmail.com>
2018-04-14 12:27:00 -0300

    funcionando

--- volrend/include/util.h
+++ volrend/include/util.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <iostream>
-#include <unistd.h>
 
-void options(int argc, char**argv);
+enum version {
+    SEQUENTIAL
+};
+
+void usage(version v, const char *name);
+void fatal(const char *str);
--- volrend/source/util.cpp
+++ volrend/source/util.cpp
@@ -1,5 +1,18 @@
 #include "util.h"
 
-void options(int argc, char**argv) {
-    // getopt
+void usage(version v, const char *name) {
+    std::cout << "Usage: ./build/apps/" << name << " [OPTIONS]...\n\n";
+    std::cout << "Options:\n";
+    std::cout << "\t -i The input to use to run the benchmarks. Default: 'test'.\n";
+    if(v != SEQUENTIAL) {
+        std::cout << "\t -n The minimum number of threads to use. Default: '1'.\n";
+    }
+    std::cout << "\t -h Displays this help message.\n";
+}
+
+void fatal(const char *str) {
+    if(str != nullptr) {
+        std::cout << "fatal: " << str << std::endl;
+    }
+    exit(1);
 }
